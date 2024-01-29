834a62cdddb61039a3b1b28a3cf1802bd4990bdd
stanlet2000 <stanlet2000@gmail.com>
2024-01-29 20:22:10 +0800

    find last price and try real time

--- real_time.py
+++ real_time.py
@@ -5,22 +5,36 @@ from bs4 import BeautifulSoup
 
 stock_url = "https://finance.yahoo.com/world-indices/"
 
-response = requests.get(stock_url)
-
-print(response)
-
-soup = BeautifulSoup(response.text, 'html.parser')
-
-table = soup.find('table', class_ = 'W(100%)')
-
-table_content = table.find('tbody')
 # print(table.text)
 target = '^GSPC'
 
-indices = table_content.find_all('tr')
+def print_price(target, check_response=False):
+    response = requests.get(stock_url)
+    if check_response:
+        print(response)
 
-for i in indices:
-    if target == i.find('td').text:
-        indice = i
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-print(indice.text)
+    table = soup.find('table', class_ = 'W(100%)')
+
+    table_content = table.find('tbody')
+
+    indices = table_content.find_all('tr')
+    for i in indices:
+        if target == i.find('td').text:
+            indice = i
+
+    elements = indice.find_all('td')
+    for element in elements:
+        if element['aria-label'] == 'Last Price':
+            print(element.text)
+    # print(indice.text)
+
+
+
+if __name__ == '__main__':
+    timestep = 10
+
+    for i in range(timestep):
+        time.sleep(5)
+        print_price(target, check_response=True)
